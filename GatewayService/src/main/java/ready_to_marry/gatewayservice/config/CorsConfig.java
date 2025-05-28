e284e17adbed6e7426d0c65636bfdcf24a742b09
Hyundoo <114637614+HyunDooBoo@users.noreply.github.com>
2025-05-28 15:13:41 +0900

    feat: local react 접속 허용

---
+++ GatewayService/src/main/java/ready_to_marry/gatewayservice/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package ready_to_marry.gatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000"); // React 개발 서버 주소
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
