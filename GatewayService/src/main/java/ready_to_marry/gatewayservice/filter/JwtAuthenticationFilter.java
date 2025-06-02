907ceb06e199a490abc91e132859b2ea1cd79b2a
Hyundoo <114637614+HyunDooBoo@users.noreply.github.com>
2025-06-02 23:50:18 +0900

    refactor: filter 전송 정보 수정

--- GatewayService/src/main/java/ready_to_marry/gatewayservice/filter/JwtAuthenticationFilter.java
+++ GatewayService/src/main/java/ready_to_marry/gatewayservice/filter/JwtAuthenticationFilter.java
@@ -57,31 +57,27 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
             switch (claims.getRole()) {
                 case "ADMIN":
                     mutatedRequestBuilder
-                            .header("X-Account-Id", claims.getAccountId())
-                            .header("X-Admin-Id", String.valueOf(claims.getAccountId()))
+                            .header("X-Admin-Id", String.valueOf(claims.getAdminId()))
                             .header("X-Admin-Role", claims.getAdminRole())
-                            .header("X-Role", claims.getAdminRole());
+                            .header("X-Role", claims.getRole());
                     System.out.println(claims.getAdminRole());
-                    System.out.println("admin id"+String.valueOf(claims.getAccountId()));
-                    System.out.println(claims.getAccountId());
-                    System.out.println(claims.getAdminRole());
+                    System.out.println(claims.getAdminId());
+                    System.out.println(claims.getRole());
                     break;
                 case "PARTNER":
                     mutatedRequestBuilder
-                            .header("X-Account-Id", claims.getAccountId())
                             .header("X-Partner-Id", String.valueOf(claims.getPartnerId()))
                             .header("X-Role", claims.getRole());
-                    System.out.println(claims.getRole());
                     System.out.println(claims.getPartnerId());
+                    System.out.println(claims.getRole());
                     break;
                 case "USER":
                 default:
                     mutatedRequestBuilder
-                            .header("X-Account-Id", claims.getAccountId())
                             .header("X-User-Id", String.valueOf(claims.getUserId()))
                             .header("X-Role", claims.getRole());
-                    System.out.println(claims.getRole());
                     System.out.println(claims.getUserId());
+                    System.out.println(claims.getRole());
                     break;
             }
 
